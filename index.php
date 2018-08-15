a576640539cc412023419bb7bb896f4728110583
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-15 19:11:25 -0400

    header start

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <html>
  <head>
   <title>PHP Test</title>
+  <style>
+  #banner{
+  	height: 25%;
+  	text-align: center;
+  }
+  #sub-header{
+  	float: left;
+  }
+  </style>
  </head>
  <body>
- <?php echo '<p>Hello World this is ryan schneiderman</p>'; ?> 
+ <div id = "banner">
+ 	<h1>RYAN SCHNEIDERMAN</h1>
+ 	<div id = "sub-header">
+ 		<p>rschneides21@gmail.com</p>
+ 		<p>(802) 777 0769</p>
+ 	</div>
+ </div>
  <a href = "test.php">test</a>
  </body>
 </html>
