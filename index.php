3d30e2c2118965698f32d718084db6d1a4272d16
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-15 19:17:15 -0400

    header start

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   	height: 25%;
   	text-align: center;
   }
-  #sub-header{
+  .sub-header{
   	float: left;
   }
   </style>
@@ -15,8 +15,8 @@
  <div id = "banner">
  	<h1>RYAN SCHNEIDERMAN</h1>
  	<div id = "sub-header">
- 		<p>rschneides21@gmail.com</p>
- 		<p>(802) 777 0769</p>
+ 		<p class = "sub-header">rschneides21@gmail.com</p>
+ 		<p class = "sub-header">(802) 777 0769</p>
  	</div>
  </div>
  <a href = "test.php">test</a>
