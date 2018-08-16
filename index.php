c143d082633a76773678210b9a43f1594b86a52c
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-16 16:14:55 -0400

    header adds

--- index.php
+++ index.php
@@ -55,6 +55,12 @@
  	<div id = "first-pic">
  		<img class = "pic-links" src = "web_design_pic.png" width = "30%" height = "auto">
  	</div>
+ 	<div id = "second-pic">
+ 		<img class = "pic-links" src = "writing.jpeg" width = "30%" height = "auto">
+ 	</div>
+ 	<div id = "third-pic" >
+ 		<img class = "pic-links" src = "bball.jpg" width = "30%" height = "auto">
+ 	</div>
  </div>	
  </div>
  </body>
