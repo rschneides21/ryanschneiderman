03f3470018d3d082fc585a3eecc6b6d72b8e3449
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-16 13:05:26 -0400

    header adds

--- index.php
+++ index.php
@@ -23,14 +23,17 @@
   	padding-right: 35%;
   	padding-left: 10px;
   }
+  #header-line{
+  	padding-top: 15px;
+  }
   </style>
  </head>
  <body>
  <div id = "banner">
  	<h1>RYAN SCHNEIDERMAN</h1>
  	<p id = "email" class = "sub-header">rschneides21@gmail.com</p>
- 	<svg height = "10" width = "1" class = "sub-header">
- 		<line x1="0" y1="0" x2="1" y2="10" style="stroke:#707070;stroke-width:2"/>
+ 	<svg height = "53" width = "1" class = "sub-header" id = "header-line">
+ 		<line x1="0" y1="0" x2="1" y2="53" style="stroke:#707070;stroke-width:2"/>
  	</svg>
  	<p id = "phone" class="sub-header">(802) 777 0769</p>
  </div>
