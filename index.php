c9630f611d0fe6c56392a259a4d0ad1bc2159482
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-16 12:58:01 -0400

    header adds

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
  	<h1>RYAN SCHNEIDERMAN</h1>
  	<p id = "email" class = "sub-header">rschneides21@gmail.com</p>
  	<svg height = "10" width = "1" class = "sub-header">
- 		<line x1="0" y1="0" x2="1" y2="10" style="stroke:rgb(255,0,0);stroke-width:2"/>
+ 		<line x1="0" y1="0" x2="1" y2="10" style="stroke:#707070;stroke-width:2"/>
  	</svg>
  	<p id = "phone" class="sub-header">(802) 777 0769</p>
  </div>
