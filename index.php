7ccb169335d51a851524bb3e58afd98c548359b8
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-17 17:39:52 -0400

    header adds

--- index.php
+++ index.php
@@ -4,108 +4,106 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <style>
   #banner{
-  	text-align: center;
-  	font-family: 'Montserrat', sans-serif;
-  	color: #3E3E3E;
+    text-align: center;
+    font-family: 'Montserrat', sans-serif;
+    color: #3E3E3E;
   }
 
   #name{
-  	font-size: 80px;
-  	font-weight: 300;
-  	margin-bottom: 20px;
+    font-size: 80px;
+    font-weight: 300;
+    margin-bottom: 20px;
   }
 
   .sub-header{
-  	text-align: center;
-  	float: left;
+    text-align: center;
+    float: left;
 
   }
   #email{
-  	padding-left: 33%;
-  	padding-right: 10px;
-  	max-width: 50%;
+    padding-left: 33%;
+    padding-right: 10px;
+    max-width: 50%;
   }
   #phone{
-  	padding-left: 10px;
-  	padding-right: 35%;
-  	max-width: 50%;
+    padding-left: 10px;
+    padding-right: 35%;
+    max-width: 50%;
   }
   #header-line{
-  	padding-top: 10px;
+    padding-top: 10px;
   }
   #second-level{
-  	padding-top: 10px;
+    padding-top: 10px;
   }
   .pic-links{
-  	float: left;
+    float: left;
   }
 
   #parallelogram {
-  	display: inline-block;
-  	position:relative;
-  	transform-origin:0 100%;
-  	transform:skewX(-7deg);
-  	overflow:hidden;
-	width: 35%;
-	height: 74.7%;
-	right: 65px;
-	z-index: 2;
-	border-left: 5px solid white;
-	border-right: 5px solid white;
+    display: block;
+    position:relative;
+    transform-origin:0 100%;
+    transform:skewX(-7deg);
+    overflow:hidden;
+  width: 35%;
+  height: 74.7%;
+  right: 65px;
+  z-index: 2;
+  border-left: 5px solid white;
+  border-right: 5px solid white;
   }
   #parallelogram:before{
-  	content:'';
-  	position:absolute;
-  	width:175%; height:100%;
-  	background-image:url('writing.jpeg');
-  	background-size:cover;
-  	background-position:center;
-  	transform-origin:inherit;
-  	transform:skewX(7deg);
+    content:'';
+    position:absolute;
+    width:175%; height:100%;
+    background-image:url('writing.jpeg');
+    background-size:cover;
+    background-position:center;
+    transform-origin:inherit;
+    transform:skewX(7deg);
   }
   #third-pic{
-  	height: 73%;
-  	width: auto;
+    position: absolute;
+    height: 73%;
+    width: auto;
+    right: 30px;
+    top: 25%;
   }
 
   #first-pic{
-  	height: 73%;
-  	width: auto;
+    height: 73%;
+    width: auto;
   }
 
   #sub-header{
-  	padding-bottom: 10px;
+    padding-bottom: 10px;
   }
 
   </style>
  </head>
  <body>
  <div id = "banner">
- 	<p id = "Name">RYAN SCHNEIDERMAN</p>
- 	<div id = "sub-header">
- 		<p id = "email" class = "sub-header">rschneides21@gmail.com</p>
- 	 	<svg height = "40" width = "1" class = "sub-header" id = "header-line">
- 			<line x1="0" y1="0" x2="1" y2="40" style="stroke:#3E3E3E;stroke-width:2"/>
- 		</svg>
-		<p id = "phone" class="sub-header">(802) 777 0769</p>
-	</div>	
+  <p id = "Name">RYAN SCHNEIDERMAN</p>
+  <div id = "sub-header">
+    <p id = "email" class = "sub-header">rschneides21@gmail.com</p>
+    <svg height = "40" width = "1" class = "sub-header" id = "header-line">
+      <line x1="0" y1="0" x2="1" y2="40" style="stroke:#3E3E3E;stroke-width:2"/>
+    </svg>
+    <p id = "phone" class="sub-header">(802) 777 0769</p>
+  </div>  
  </div>
  <div id = "second-level">
- 	<table>
- 		<tr>
- 			<td>
- 				<img id = "first-pic" src = "web_design_pic.png">
- 			</td>
- 			<td>
- 				<div id = parallelogram>
- 			</td>
- 			<td>
- 				<img id = "third-pic" src = "bball.jpg">
- 			</td>	
- 		</tr>
- 	</table>
- 	
- </div>	
+  <div>
+    <img id = "first-pic" class = "pic-links" src = "web_design_pic.png" >
+  </div>
+  <div id = "parallelogram" class = "pic-links">
+  </div>
+  <div>
+    <img id = "third-pic" class = "pic-links" src = "bball.jpg" >
+  </div>  
+  
+ </div> 
 
  </body>
 </html>
