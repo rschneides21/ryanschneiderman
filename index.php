94d90cfa861bd8372288c8f542fe5e30b6472865
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-20 16:43:25 -0400

    header adds

--- index.php
+++ index.php
@@ -147,11 +147,11 @@
  		shiftPic3();
  		$("second-level").css('margin', 'auto')
 	});
-	$(document).ready(function() {
+
 		$("#second-pic").css("width", $("#first-pic").width() * 1.04);
 		shiftPic2();
 		shiftPic3();
-	}
+	
  	
 	
 
