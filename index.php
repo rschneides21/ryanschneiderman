8bb7d9cdb73a622653255fae81bde61b75f9e49d
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-20 14:10:08 -0400

    header adds

--- index.php
+++ index.php
@@ -146,7 +146,7 @@
 	});
 	console.log("width 1: ", $("#first-pic").width());
 	console.log("width 2: ", $("#second-pic").width());
-	console.log("width 3: ", $("#third-pic").width())
+	console.log("width 3: ", $("#third-pic").width());
 	shiftPic2();
 	shiftPic3();
 
