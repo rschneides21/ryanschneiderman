57b12f8a42881c5cd44d2f1a63c7559bfc9c2589
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-20 18:45:05 -0400

    header adds

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
     position:absolute;
     width:175%; height:100%;
     background-image:url('webdesign2.png');
-    background-size: 110% 100%;
+    background-size: 110%;
     transform-origin:inherit;
     transform:skewX(7deg);
   }
