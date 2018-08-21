f95ecb2ac106d09ee786383c29d89cbce0a3a8bb
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-21 17:33:10 -0400

    header adds

--- index.php
+++ index.php
@@ -89,14 +89,12 @@
   	transition: .5s ease;
   	opacity: 0;
   	position: absolute;
-  	top: 50%;
-  	left: 50%;
   	transform: translate(-50%, -50%);
   	-ms-transform: translate(-50%, -50%);
   	text-align: center;
   }
 
-  .image-container:hover .image {
+  .image-container:hover .pic-links {
   		opacity: .3;
 	}
 
