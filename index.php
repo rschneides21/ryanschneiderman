70e47fc282836eb9cbeb19247e8873abae754e7c
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-23 11:49:57 -0400

    line fix

--- index.php
+++ index.php
@@ -212,6 +212,7 @@
       	$("#second-pic").css("height", $("#first-pic").height());
 	  	$("#third-pic").css("height", $("#first-pic").height());
 	  	$("#third-pic").css("width", $("#first-pic").width());
+	  	$(".middle".css("top", $("#first-pic").position().top + 15))
 
 		shiftPic2();
 		shiftPic3();
