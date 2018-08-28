a112c5d5f6474ebb2f845b0c72010f7561a613f8
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-28 12:19:55 -0400

    test

--- test.php
+++ test.php
@@ -171,7 +171,15 @@
  	class LeftImage extends React.Component {
  		render(){
  			return(
- 				<div> HELLO WORLD </div>		
+ 				<div className = "image-container">
+ 					<a href = "test.php">
+ 						<img className = "pic-links" id = "first-pic" src = "writing-copy.jpg"/>
+ 					</a>
+ 					<div className = "middle" id = "mid1">
+ 						<a herf = "test.php"> WRITING
+ 						</a>
+ 					</div>
+ 				</div>
  			);
  		}
  	}
