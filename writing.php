2f15851e250f5c529f63b95b662b915ea81bb073
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-30 13:23:32 -0400

    test

--- writing.php
+++ writing.php
@@ -54,15 +54,9 @@
 	<body>
 		<div id = "body">
 			<div id = "header">
-				<table style="margin:auto">
-					<tr>
-						<td><div class = "header">Writing</div></td>
-					</tr>	
-					<tr>
-						<td><div id = "article-header" class = "sub-header">articles</div></td>
-						<td><div id = "essay-header" class = "sub-header">essays</div></td>
-					</tr>
-				</table>
+				<div class = "header">Writing</div>
+				<div id = "article-header" class = "sub-header">articles</div>
+				<div id = "essay-header" class = "sub-header">essays</div>
 				<svg style = "height: 0; width: 80%; margin: auto;" id = "header-line">
       				<line x1="0" y1="0" x2="100%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
     			</svg>
