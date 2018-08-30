0fbdaa2305ce6d78b5c871eb8d0d7a8a9f130784
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-30 10:39:46 -0400

    test

--- writing.php
+++ writing.php
@@ -32,8 +32,8 @@
 				<h1 class = "header">Writing</h1>
 				<div class = "sub-header">articles</div>
 				<div class = "sub-header">essays</div>
-				<svg style = "height: 1px; width: 80%;" class = "sub-header" id = "header-line">
-      				<line x1="0" y1="0" x2="80%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
+				<svg style = "height: 1px; width: 80%;" id = "header-line">
+      				<line x1="0" y1="0" x2="100%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
     			</svg>
 			</div>	
 		</div>
