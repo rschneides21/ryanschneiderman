c549c48bf208b49729a6e66303c109cd9cd94c4d
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-10 16:45:19 -0400

    web-dev

--- web_dev.php
+++ web_dev.php
@@ -72,7 +72,7 @@
       }
 
       #techs{
-        margin: auto;
+        padding-left: 20%;
       }
 
 
@@ -112,7 +112,7 @@
         </div>
         <div id = "technologies">
           <div class = "header2">Technologies</div>
-          <div id = "techs"> HTML/CSS/Javascript &nbsp | &nbsp  React  &nbsp | &nbsp  Bootstrap  &nbsp | &nbsp  Django  &nbsp | &nbsp  Python  &nbsp | &nbsp  Java  &nbsp | &nbsp  PHP </div>
+          <p id = "techs"> HTML/CSS/Javascript &nbsp | &nbsp  React  &nbsp | &nbsp  Bootstrap  &nbsp | &nbsp  Django  &nbsp | &nbsp  Python  &nbsp | &nbsp  Java  &nbsp | &nbsp  PHP </p>
           <ul>
             <li>HTML/CSS/Javascript</li>
             <li>React</li>
