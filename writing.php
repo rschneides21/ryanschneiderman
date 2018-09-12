f1065fd4edc4928379bbb7a930ab96ac4ef2e0dd
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-12 17:48:10 -0400

    sidebar

--- writing.php
+++ writing.php
@@ -31,7 +31,7 @@
   		}
 
   		#header-line{
-  			padding-left: 30%;
+  			padding-left: 17%;
   		}
 
   		#article-header{
@@ -101,8 +101,8 @@
 			</div>
 			<div id = "headers">
 				<div id = "header" >Writing</div>
-				<svg style = "height: 1px; width: 80%;" >
-      				<line id = "header-line" x1="0" y1="0" x2="100%" y2="1" style="stroke:#3E3E3E;stroke-width:2"/>
+				<svg style = "height: 1px; width: 80%;" id = "header-line">
+      				<line  x1="0" y1="0" x2="100%" y2="1" style="stroke:#3E3E3E;stroke-width:2"/>
     			</svg>
 			</div>
 			<div class = "header2">Sports Columnist Tufts Daily (2016 - 2017)</div>
