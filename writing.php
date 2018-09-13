f2391d024562312688b4a247d8964e73858c40e9
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-13 15:16:00 -0400

    sidebar

--- writing.php
+++ writing.php
@@ -129,7 +129,7 @@
 				<div id = "header" >Writing</div>
 				<div id = "line">
 					<svg style = "height: 5px; width: 100%;" id = "header-line">
-      					<line  x1="0" y1="0" x2="100%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
+      					<line  x1="0" y1="0" x2="100%" y2="5" style="stroke:#3E3E3E;stroke-width:2"/>
     				</svg>
     			</div>	
 			</div>
