c9f71b31da4977c918d2a50d2f247e8df49e19ba
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-13 15:12:05 -0400

    sidebar

--- writing.php
+++ writing.php
@@ -60,7 +60,7 @@
         	font-size: 1.2vw;
         	width: 13%;
         	height:100%;
-        	background-color: #3E3E3E;;
+        	background-color: #3E3E3E;
       	}
 
       	.sidebar{
@@ -128,7 +128,7 @@
 			<div id = "headers">
 				<div id = "header" >Writing</div>
 				<div id = "line">
-					<svg style = "height: 1px; width: 100%;" id = "header-line">
+					<svg style = "height: 5px; width: 100%;" id = "header-line">
       					<line  x1="0" y1="0" x2="100%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
     				</svg>
     			</div>	
