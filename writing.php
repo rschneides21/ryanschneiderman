b176c34511446fc0da3285720ef308480ea7c292
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-13 15:04:27 -0400

    sidebar

--- writing.php
+++ writing.php
@@ -32,7 +32,7 @@
   		}
 
   		#header-line{
-  			padding-left: 13%;
+  			padding-left: 17%;
   		}
 
   		#article-header{
@@ -128,7 +128,7 @@
 			<div id = "headers">
 				<div id = "header" >Writing</div>
 				<div id = "line">
-					<svg style = "height: 1px; width: 82%;" id = "header-line">
+					<svg style = "height: 1px; width: 80%;" id = "header-line">
       					<line  x1="0" y1="0" x2="100%" y2="0" style="stroke:#3E3E3E;stroke-width:2"/>
     				</svg>
     			</div>	
