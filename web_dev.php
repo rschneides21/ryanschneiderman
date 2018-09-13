1318f562172703f1a0d167aaea11534c90c6afdb
Ryan S <ryan.schneiderman@tufts.edu>
2018-09-13 17:27:50 -0400

    sidebar

--- web_dev.php
+++ web_dev.php
@@ -75,6 +75,73 @@
         text-align: center;
       }
 
+       #sidebar{
+          color: white;
+          position: fixed;
+          font-size: 1.2vw;
+          width: 15%;
+          height:100%;
+          background-color: #3E3E3E;
+        }
+
+      p {
+        text-align: justify;
+        padding-left: 17%;
+        padding-right: 15%;
+      }
+
+      .sidebar{
+          padding-left: 5%;
+          padding-top: 3%;
+
+        }
+
+        #sidebar-nav{
+          padding-top: 40%;
+        }
+
+        .sidebar-home{
+          padding-left: 5%;
+          padding-bottom: 3%;
+        }
+
+        #sidebar-line{
+          padding-left: 5%;
+      }
+
+      a.sidebar-link:visited {
+        text-decoration: none;
+        color: white;
+      }
+
+      a.sidebar-link:link {
+        text-decoration: none;
+        color: white;
+      }
+
+      a.sidebar-link:hover {
+        text-decoration: none;
+        color: #85e8f1;
+      }
+
+      a#selected:link {
+        color: #85e8f1;
+      }
+
+      a#selected:visited{
+        color: #85e8f1;
+      }
+
+      #taper{
+          position: fixed;
+          width: 0;
+          height: 0;
+          border-style: solid;
+          border-color: transparent transparent white transparent;
+          z-index: 2;
+          left: 12%;
+        }
+
 
 
   		</style>
@@ -82,6 +149,18 @@
 
 	<body>
 		<div id = "body">
+      <div id = "sidebar">
+        <div id = "sidebar-nav">
+          <div class = "sidebar-home"><a class = "sidebar-link" href = "index.php">Home</a></div>
+          <svg style = "height: 1px; width: 80%;" >
+                <line id = "sidebar-line" x1="0" y1="0" x2="100%" y2="0" style="stroke:white;stroke-width:2"/>
+            </svg>
+          <div class = "sidebar"><a class = "sidebar-link" href = "writing.php">Writing</a></div>
+          <div class = "sidebar"><a class = "sidebar-link" href = "web_dev.php">Web Development</a></div>
+          <div class = "sidebar"><a class = "sidebar-link" id = "selected" href = "basketball.php">Coaching</a></div>
+        </div>
+      </div>
+      <div id = "taper"></div>
 			<div id = "headers">
 				<div id = "header" >Web Development</div>
 				<svg style = "height: 1; width: 80%;" id = "header-line">
